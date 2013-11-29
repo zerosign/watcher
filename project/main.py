1082339e66dd08167c2e7885e78e94f883dcacaa
zerosign <r1nlx0@gmail.com>
2013-11-29 18:51:34 +0700

    add rendering logic

--- project/main.py
+++ project/main.py
@@ -2,26 +2,37 @@
 import sys
 import os
 import json
+import argparse
 
 from watcher.watcher import Watcher
+from watcher.render import Renderer
+
+
+parser = argparse.ArgumentParser(description="Watcher for a process")
+
+def init_args():
+	
+	parser.add_argument('--action', nargs='?', default='watch', help='an action to do (watch|render)')
+
+	parser.add_argument('--config', nargs='?', default="config.json", help='file config to be parsed')
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Error : Argument needs more")
+	
+	init_args()
+	args = vars(parser.parse_args())
+	
+	if not os.path.exists(args['config']):
 		sys.exit(-1)
 
-	path = sys.argv[1]
+	if args['action'] == 'watch':
+		config = Watcher.parse(json.loads("".join(open(args['config'], 'r').readlines())))
+		watchers = [Watcher(conf) for conf in config]
+		processes = [watcher.start() for watcher in watchers]
 	
-	print("Using config path : %s " % path) 
-	
-	if not os.path.exists(path):
-		sys.exit(-1)
-	
-	config = Watcher.parse(json.loads("".join(open(path, 'r').readlines())))
-	watchers = [Watcher(conf) for conf in config]
-	processes = [watcher.start() for watcher in watchers]
-	
-	# join all process
-	for process in processes:
-		process.join()
+		# join all process
+		for process in processes:
+			process.join()
 
+	elif args['action'] == 'render':
+		renderer = Renderer()
+		renderer.render(args[path])
---
+++ project/watcher/render.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plot
+
+class Renderer():
+	
+	def render(path=None):
+		print("Rendering the output of %s" % (path))
+		f = open(path)
+		f.close()
+		pass
+

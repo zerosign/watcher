de317eec78b99f022fad5148043d117b8e81d883
zerosign <r1nlx0@gmail.com>
2013-12-04 10:48:53 +0700

    fix stupid indent (because different config between computer)

--- project/watcher/watcher.py
+++ project/watcher/watcher.py
@@ -9,68 +9,71 @@ from .helper import TimeHelper
 from .stats import Stats, EmptyStats
 
 class Watcher():
-	
-	output_folder = None
 
-	def __init__(self, config=()):
-		self.pid = int(config[0][0])
-		self.name = config[0][1]
-		self.config = config[1]
-		filename =  "%s/%s-%d.output" % (Watcher.output_folder, self.name, self.pid)
-		existed = os.path.exists(filename)
-		self.output = open(filename, 'a')
+    output_folder = None
 
-		if not existed:
-			self.output.write(Stats.header())
+    def __init__(self, config=()):
+        self.pid = int(config[0][0])
+        self.name = config[0][1]
+        self.config = config[1]
+        filename =  "%s/%s-%d.output" % (Watcher.output_folder, 
+                                         self.name, self.pid)
+        existed = os.path.exists(filename)
+        self.output = open(filename, 'a')
 
-	@staticmethod
-	def fetch(watcher):
-		stats = EmptyStats()
-		try: 
-			p = psutil.Process(watcher.pid)
-                        mem_usage_total = sum(p.get_memory_info())
-			stats = Stats(watcher.name, watcher.pid, mem_usage_total, p.get_cpu_percent(1), 
-					len(p.get_threads()), p.get_num_fds(),
-					len(p.get_connections()), time.time())
-			
-		except Exception as ex:
-			print(ex)
-		
-		return stats
+        if not existed:
+            self.output.write(Stats.header())
 
-	@staticmethod
-	def watch(watcher):
-		timespan = TimeHelper.parse(watcher.config["timespan"])
-		start = datetime.now()
-		while True:
-			result = (datetime.now() - start) < timespan
-			# print(result)
-			if not result:
-				break
-			stats = Watcher.fetch(watcher)
-			if not isinstance(stats, EmptyStats):
-				# print( "%s\n" % str(stats) )
-				watcher.output.write("%s\n" % str(stats))
-				watcher.output.flush()
-			else:
-				break
+    @staticmethod
+    def fetch(watcher):
+        stats = EmptyStats()
+        try: 
+            p = psutil.Process(watcher.pid)
+            mem_usage_total = sum(p.get_memory_info())
+            stats = Stats(watcher.name, watcher.pid,
+                          mem_usage_total, 
+                          p.get_cpu_percent(1), 
+                          len(p.get_threads()), p.get_num_fds(),
+                          len(p.get_connections()), time.time())
 
-		watcher.output.close()
+        except Exception as ex:
+            print(ex)
+            return stats
 
-	def start(self):
-		self.process = Process(target=Watcher.watch, args = [self])
-		self.process.start()
-		return self.process
+    @staticmethod
+    def watch(watcher):
+        timespan = TimeHelper.parse(watcher.config["timespan"])
+        start = datetime.now()
+        while True:
+            result = (datetime.now() - start) < timespan
+            # print(result)
+            if not result:
+                break
+            stats = Watcher.fetch(watcher)
+            if not isinstance(stats, EmptyStats):
+                # print( "%s\n" % str(stats) )
+                watcher.output.write("%s\n" % str(stats))
+                watcher.output.flush()
+            else:
+                break
 
-	@staticmethod
-	def parse(config={}):
-		config_new = []
-		proc_names = [proc_name[0] for proc_name in config.items()]
-		for proc in psutil.process_iter():
-			if proc.name in proc_names:
-				config_new.append(((proc.pid, proc.name), config[proc.name]))
-		return config_new
+            watcher.output.close()
 
-	
-	def __str__(self):
-		return ",".join([self.pid, self.name, self.config])
+    def start(self):
+        self.process = Process(target=Watcher.watch, args = [self])
+        self.process.start()
+        return self.process
+
+    @staticmethod
+    def parse(config={}):
+        config_new = []
+        proc_names = [proc_name[0] for proc_name in config.items()]
+        for proc in psutil.process_iter():
+            if proc.name in proc_names:
+                config_new.append(((proc.pid, proc.name), 
+                                   config[proc.name]))
+                return config_new
+
+
+    def __str__(self):
+        return ",".join([self.pid, self.name, self.config])
